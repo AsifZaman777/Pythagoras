f9813e1e34555e34b9bbc48f64ab042528d71af0
Hasansarwer <hasanmathju@gmal.com>
2024-02-15 13:38:45 +0600

    complete need to add math equation

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = new PIXI.Application({
   resizeTo: window,
 });
 document.body.appendChild(app.view);
-
+const position = {
+  x: app.renderer.width / 2,
+  y: app.renderer.height / 2,}
+  ;
 const labelContainer = new PIXI.Container();
 app.stage.addChild(labelContainer);
 
@@ -30,30 +33,12 @@ let a = 200;
 let b = 100;
 let squareLength = a + b;
 
-
 const triangle1 = new PIXI.Graphics();
 const triangle2 = new PIXI.Graphics();
 const triangle3 = new PIXI.Graphics();
 const triangle4 = new PIXI.Graphics();
 const shapeContainer = new PIXI.Graphics();
-// Trtangle style
 
-triangle1.beginFill(0x8d1414); // Fill color
-triangle1.lineStyle(2, 0xffffff); // Line style
-triangle2.beginFill(0x8d1414); // Fill color
-triangle2.lineStyle(2, 0xffffff);
-triangle3.beginFill(0x8d1414); // Fill color
-triangle3.lineStyle(2, 0xffffff);
-triangle4.beginFill(0x8d1414); // Fill color
-triangle4.lineStyle(2, 0xffffff);
-shapeContainer.beginFill(0xa485b3);
-shapeContainer.lineStyle(2, 0xffffff);
-// shapeContainer.endFill();
-app.stage.addChild(shapeContainer);
-shapeContainer.addChild(triangle1);
-shapeContainer.addChild(triangle2);
-shapeContainer.addChild(triangle3);
-shapeContainer.addChild(triangle4);
 
 const controlPoint = new PIXI.Graphics();
 controlPoint.beginFill(0x0000ff); // Color of the control point
@@ -61,8 +46,29 @@ controlPoint.drawCircle(0, -2, 10); // Adjust size as needed
 controlPoint.endFill();
 controlPoint.interactive = true;
 controlPoint.buttonMode = true;
-controlPoint.x = squareLength;
-controlPoint.y = squareLength;
+
+const controlPoint1 = new PIXI.Graphics();
+controlPoint1.beginFill(0x0000ff); // Color of the control point
+controlPoint1.drawCircle(0, -2, 10); // Adjust size as needed
+controlPoint1.endFill();
+controlPoint1.interactive = true;
+controlPoint1.buttonMode = true;
+
+const controlPoint2 = new PIXI.Graphics();
+controlPoint2.beginFill(0x0000ff); // Color of the control point
+controlPoint2.drawCircle(0, -2, 10); // Adjust size as needed
+controlPoint2.endFill();
+controlPoint2.interactive = true;
+controlPoint2.buttonMode = true;
+
+app.stage.addChild(shapeContainer);
+shapeContainer.addChild(triangle1);
+shapeContainer.addChild(triangle2);
+shapeContainer.addChild(triangle3);
+shapeContainer.addChild(triangle4);
+shapeContainer.addChild(controlPoint);
+shapeContainer.addChild(controlPoint1);
+shapeContainer.addChild(controlPoint2);
 
 //button create
 const submitButton = new PIXI.Graphics();
@@ -70,14 +76,25 @@ submitButton.beginFill(0x008000);
 submitButton.drawRect(0, 0, 400, 60); // Adjust size as needed
 submitButton.position.set(
   (app.renderer.width - 350) / 2,
-  app.renderer.height - 900
+  app.renderer.height - 200
 ); // Position at the bottom center
 submitButton.interactive = true;
 submitButton.buttonMode = true;
 
+//button create
+const resetButton = new PIXI.Graphics();
+resetButton.beginFill(0x008000);
+resetButton.drawRect(0, 0, 400, 60); // Adjust size as needed
+resetButton.position.set(
+  (app.renderer.width - 350) / 2,
+  app.renderer.height - 100
+); // Position at the bottom center
+resetButton.interactive = true;
+resetButton.buttonMode = true;
+
 // Add the control point to the shape container
-shapeContainer.addChild(controlPoint);
-shapeContainer.addChild(controlPoint);
+
+// shapeContainer.addChild(controlPoint);
 
 // Text style for the button label
 const buttonTextStyle = new PIXI.TextStyle({
@@ -91,16 +108,50 @@ const buttonText = new PIXI.Text(
   "\nপিথাগোরাসের প্রতিপাদন দেখুন\n",
   buttonTextStyle
 );
+const rersetButtonText = new PIXI.Text(
+  "\nপুনরায় দেখুন\n",
+  buttonTextStyle
+);
 buttonText.position.set(
   submitButton.width / 2 - buttonText.width / 2,
   submitButton.height / 2 - buttonText.height / 2
 );
+rersetButtonText.position.set(
+  resetButton.width / 2 - rersetButtonText.width / 2,
+  resetButton.height / 2 - rersetButtonText.height / 2
+);
 submitButton.addChild(buttonText);
+resetButton.addChild(rersetButtonText);
+app.stage.addChild(resetButton);
 app.stage.addChild(submitButton);
 
 
 // rebuild the triangle and square
 function drawElements(a,b,squareLength){
+
+  // Remove the previous shapes
+  // app.stage.removeChild(shapeContainer);
+  // Trtangle style
+
+triangle1.clear();
+triangle2.clear();
+triangle3.clear();
+triangle4.clear();
+shapeContainer.clear();
+
+
+triangle1.beginFill(0x8d1414); // Fill color
+triangle1.lineStyle(1, 0xffffff); // Line style
+triangle2.beginFill(0x8d1414); // Fill color
+triangle2.lineStyle(1, 0xffffff);
+triangle3.beginFill(0x8d1414); // Fill color
+triangle3.lineStyle(1, 0xffffff);
+triangle4.beginFill(0x8d1414); // Fill color
+triangle4.lineStyle(1, 0xffffff);
+shapeContainer.beginFill(0xa485b3);
+shapeContainer.lineStyle(2, 0xffffff);
+// shapeContainer.endFill();
+
 
   // Draw the triangles
   triangle1.drawPolygon([0, 0, 0, b, a, b]);
@@ -111,37 +162,78 @@ function drawElements(a,b,squareLength){
   // Draw the outersquare
 
   shapeContainer.drawPolygon([0, 0, 0, a + b, a + b, a + b, a + b, 0]);
-  shapeContainer.x = app.stage.width / 2 - squareLength / 2;
-  shapeContainer.y = app.stage.height / 2 + squareLength/2;
+  shapeContainer.x = position.x - squareLength/2 ;
+  shapeContainer.y = position.y / 2  ;
+  // console.log('scp: ', shapeContainer.y, app.stage.height / 2, squareLength/2);
 
    //position define in the square
   triangle1.y = a;
+  triangle1.x = 0;
   triangle2.x = a;
   triangle2.y = b;
   triangle3.x = b;
+  triangle3.y = 0;
+  triangle4.y = 0;
+  triangle4.x = 0;
+  controlPoint.x = squareLength;
+  controlPoint.y = squareLength;
+  controlPoint1.x = a;
+  controlPoint1.y = squareLength;
+  controlPoint2.x = 0;
+  controlPoint2.y = a;
 }
 
 // Event listener to handle stretching behavior
 controlPoint.on("pointerdown", (event) => {
-  const originalPosition = event.data.getLocalPosition(shapeContainer.parent);
-  const originalSize = {
-    width: shapeContainer.width,
-    height: shapeContainer.height,
-  }; // Get the initial size of the square
-
+  
   const onPointerMove = (event) => {
-    const newPosition = event.data.getLocalPosition(shapeContainer.parent); // Get the current position of the pointer
+    const newPosition = event.data.getLocalPosition(controlPoint.parent); // Get the current position of the pointer
+    let newSquareLength = Math.min((Math.max(newPosition.x, newPosition.y,100)),600);
+    a = (a*newSquareLength/(squareLength));
+    b = newSquareLength - a;
+    squareLength = newSquareLength;
+    shapeContainer.clear();
+    drawElements(a,b,squareLength);
+  };
 
-    // Calculate the difference in position to determine stretching
-    const dx = newPosition.x - originalPosition.x;
-    const dy = newPosition.y - originalPosition.y;
-    console.log(dx, dy);
-    // const findMax = Math.max(Math.abs(dy), Math.abs(dx));
-    Math.abs(dy) > Math.abs(dx) ? (findMax = dy) : (findMax = dx);
-    console.log(a,b,newPosition, originalPosition);
-    // Update the size of the square based on the pointer movement
-    shapeContainer.width = Math.max(originalSize.width + findMax, 100);
-    shapeContainer.height = Math.max(originalSize.height + findMax, 100);
+  const onPointerUp = () => {
+    app.renderer.plugins.interaction.off("pointermove", onPointerMove);
+    app.renderer.plugins.interaction.off("pointerup", onPointerUp);
+  };
+
+  app.renderer.plugins.interaction.on("pointermove", onPointerMove);
+  app.renderer.plugins.interaction.on("pointerup", onPointerUp);
+});
+
+// Event listener to handle stretching behavior of triangle
+controlPoint1.on("pointerdown", (event) => {
+  
+  const onPointerMove = (event) => {
+    const newPosition = event.data.getLocalPosition(controlPoint1.parent); // Get the current position of the pointer
+    a = Math.min((Math.max(newPosition.x,50)),squareLength-50);
+    b = squareLength - a;
+    shapeContainer.clear();
+    drawElements(a,b,squareLength);
+  };
+
+  const onPointerUp = () => {
+    app.renderer.plugins.interaction.off("pointermove", onPointerMove);
+    app.renderer.plugins.interaction.off("pointerup", onPointerUp);
+  };
+
+  app.renderer.plugins.interaction.on("pointermove", onPointerMove);
+  app.renderer.plugins.interaction.on("pointerup", onPointerUp);
+});
+
+// Event listener to handle stretching behavior of triangle 2
+controlPoint2.on("pointerdown", (event) => {
+  
+  const onPointerMove = (event) => {
+    const newPosition = event.data.getLocalPosition(controlPoint2.parent); // Get the current position of the pointer
+    a = Math.min((Math.max(newPosition.y,50)),squareLength-50);
+    b = squareLength - a;
+    shapeContainer.clear();
+    drawElements(a,b,squareLength);
   };
 
   const onPointerUp = () => {
@@ -171,7 +263,10 @@ submitButton.on("pointerdown", () => {
       // Move triangle1 diagonally down-right
       triangle1.y = a - (tick * a) / tf;
       triangle1.x = (tick * b) / tf;
-
+      controlPoint1.y = a - (tick * a) / tf;
+      controlPoint1.x = (tick * b) / tf;
+      controlPoint2.y = a - (tick * a) / tf;
+      controlPoint2.x = (tick * b) / tf;
       triangle2.x = a - (tick * a) / tf;
       triangle4.y = (tick * b) / tf;
     }
@@ -183,76 +278,21 @@ submitButton.on("pointerdown", () => {
   //   bsquare.x = triangle1.width / 2-60 ;
   //   bsquare.y = triangle1.height / 2;
   //   triangle1.addChild(bsquare);
+  controlPoint.interactive = false;
+  controlPoint1.interactive = false;
+  controlPoint2.interactive = false;
+  submitButton.interactive = false;
 });
 
+// Event listener for the reset button
+resetButton.on("pointerdown", () => {
+  // Remove the previous shapes
+  shapeContainer.clear();
+  drawElements(a,b,squareLength);
+  controlPoint.interactive = true;
+  controlPoint1.interactive = true;
+  controlPoint2.interactive = true;
+  submitButton.interactive = true;
+});
 
 drawElements(a,b,squareLength);
-
-triangleBaseControler = false;
-triangleHeightControler = false;
-
-// add two control points for the triangle1 to change the a and b value 
-const controlPoint1 = new PIXI.Graphics();
-controlPoint1.beginFill(0x0000FF); // Color of the control point
-controlPoint1.drawCircle(-15, -5, 10); // Adjust size as needed
-controlPoint1.endFill();
-controlPoint1.interactive = true;
-controlPoint1.buttonMode = true;
-controlPoint1.position.set(triangle1.width, triangle1.height); // Position at the bottom-right corner
-triangle1.addChild(controlPoint1);
-
-// Event listener to adjust only horizontal value
-
-
-let controlPoint1Check=controlPoint1.on('pointerdown', (event) => {
-  const originalPosition = event.data.getLocalPosition(triangle1.parent);
-  const originalWidth = triangle1.width; // Get the initial width of the triangle
-
-  const onPointerMove = (event) => {
-      const newPosition = event.data.getLocalPosition(triangle1.parent); // Get the current position of the pointer
-      const dx = newPosition.x - originalPosition.x; // Calculate the horizontal movement
-
-      // Update the width of the triangle based on the horizontal movement
-      triangle1.width = Math.max(originalWidth + dx, 100); // Ensure the width doesn't become negative
-     
-      // dont cross the square length
-      if(triangle1.width > squareLength){
-        triangle1.width = squareLength;
-      }
-      //dont cross triangle2 
-      if(triangle1.width> triangle2.x){
-        triangle1.width = triangle2.x+20;
-      }
-  };
-
-  const onPointerUp = () => {
-      app.renderer.plugins.interaction.off('pointermove', onPointerMove);
-      app.renderer.plugins.interaction.off('pointerup', onPointerUp);
-  };
-
-  app.renderer.plugins.interaction.on('pointermove', onPointerMove);
-  app.renderer.plugins.interaction.on('pointerup', onPointerUp);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
